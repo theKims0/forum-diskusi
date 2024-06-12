dc8bc20fdea16419a4c9431428781b889c26c66a
theKims0 <abdulkhakim0707@gmail.com>
2024-06-13 03:35:43 +0700

    comment secenario3

--- src/components/LoginInput.test.jsx
+++ src/components/LoginInput.test.jsx
@@ -1,6 +1,15 @@
+/**
+ * testing secenario for component login input
+ * should handle username typing correctly
+ * should handle password typing correctly
+ * should call login function when login button is clicked
+ */
 import React from 'react';
-import { describe, it, expect } from 'vitest';
+import {
+  describe, it, expect, vi,
+} from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import matchers from '@testing-library/jest-dom/matchers';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,6 +20,65 @@ import store from '../states';
 expect.extend(matchers);
 
 describe('LoginInput component', () => {
+  it('should handle username typing correctly', async () => {
+    // Arrange
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <LoginInput login={() => {}} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const usernameInput = await screen.getByPlaceholderText('Email');
+
+    // Action
+    await userEvent.type(usernameInput, 'abdul11@gmail.com');
+
+    // Assert
+    expect(usernameInput).toHaveValue('abdul11@gmail.com');
+  });
+  it('should handle password typing correctly', async () => {
+    // Arrange
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <LoginInput login={() => {}} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const passwordInput = await screen.getByPlaceholderText('Password');
+
+    // Action
+    await userEvent.type(passwordInput, 'passwordtest');
+
+    // Assert
+    expect(passwordInput).toHaveValue('passwordtest');
+  });
+  it('should call login function when login button is clicked', async () => {
+    // Arrange
+    const mockLogin = vi.fn();
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <LoginInput login={mockLogin} />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const emailInput = await screen.getByPlaceholderText('Email');
+    await userEvent.type(emailInput, 'abdul11@gmail.com');
+    const passwordInput = await screen.getByPlaceholderText('Password');
+    await userEvent.type(passwordInput, 'passwordtest');
+    const loginButton = await screen.getByRole('button', { name: 'Login' });
+
+    // Action
+    await userEvent.click(loginButton);
+
+    // Assert
+    expect(mockLogin).toHaveBeenCalledWith({
+      email: 'abdul11@gmail.com',
+      password: 'passwordtest',
+    });
+  });
   it('renders correctly', () => {
     render(
       <Provider store={store}>
